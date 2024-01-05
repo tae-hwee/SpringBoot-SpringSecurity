f4f3f5cc69a0962b8455e775b25abb218f27fd98
taehwee_park <hweetae@gmail.com>
2024-01-05 18:59:21 +0900

    [New Feature] 권한 제어 적용
    description:
    - 동일 url 내에서 method별로 권한 제어 적용

--- src/main/java/com/ted/securitypractice/api/book/BookController.java
+++ src/main/java/com/ted/securitypractice/api/book/BookController.java
@@ -7,6 +7,8 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.apache.coyote.Response;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -18,11 +20,13 @@ public class BookController {
 	private final BookService bookService;
 
 	@GetMapping("/author")
+	@PreAuthorize("hasAnyRole('USER', 'ADMIN')")
 	public ResponseEntity<List<String>> getAllAuthorNames() {
 		return ResponseEntity.ok().body(bookService.getAllAuthorNames());
 	}
 
 	@PostMapping("/author")
+	@PreAuthorize("hasRole('ADMIN')")
 	public ResponseEntity<Author> createAuthor(@Valid @RequestBody AuthorDto authorDto) {
 		return ResponseEntity.ok(bookService.createAuthor(authorDto));
 	}
